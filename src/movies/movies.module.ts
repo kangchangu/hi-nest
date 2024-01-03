d2928951430ef38f016c16e4f3b35d359a1173ce
kangchangu <lyhand@naver.com>
2024-01-03 16:47:08 +0900

    feat : movie add

---
+++ src/movies/movies.module.ts
@@ -0,0 +1,14 @@
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+import { MoviesController } from './movies.controller';
+import { MoviesService } from './movies.service';
+
+@Module({
+  imports: [],
+  controllers: [MoviesController],
+  providers: [MoviesService],
+})
+export class MoviesModule {}
